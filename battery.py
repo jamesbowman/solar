c63de39d9fa5dbff8961c9a22ec54d180e39f0d0
James Bowman <jamesb@excamera.com>
2020-04-28 07:30:04 -0700

    json dump

--- battery.py
+++ battery.py
@@ -1,4 +1,5 @@
 import time
+import json
 from renogy_rover import RenogyRover
 
 if __name__ == "__main__":
@@ -10,3 +11,25 @@ if __name__ == "__main__":
         rover.battery_voltage(),
         round(rover.solar_voltage() * rover.solar_current(), 2)
     ])
+    with open('instantaneous.json', 'w') as f:
+        json.dump([
+            ('Battery %',                             rover.battery_percentage(),          '%'),
+            ('Battery Type',                          rover.battery_type(),                ''),
+            ('Battery Capacity',                      rover.battery_capacity(),            'Ah'),
+            ('Battery Voltage',                       rover.battery_voltage(),             'V'),
+            ('Battery Temperature',                   rover.battery_temperature(),         'C'),
+            ('Controller Temperature',                rover.controller_temperature(),      'C'),
+            ('Load Voltage',                          rover.load_voltage(),                'V'),
+            ('Load Current',                          rover.load_current(),                'A'),
+            ('Load Power',                            rover.load_power(),                  'W'),
+            ('Charging Status',                       rover.charging_status_label(),       ''),
+            ('Solar Voltage',                         rover.solar_voltage(),               'V'),
+            ('Solar Current',                         rover.solar_current(),               'A'),
+            ('Solar Power',                           rover.solar_power(),                 'W'),
+            ('Energy Generated Today',                rover.energy_generation_today(),     'Wh'),
+            ('Energy Consumed Today',                 rover.energy_consumption_today(),    'Wh'),
+            ('Charging Today',                        rover.charging_amp_hours_today(),    'Ah'),
+            ('Discharging Today',                     rover.discharging_amp_hours_today(), 'Ah'),
+            ('Minimum Battery Voltage Today',         rover.battery_min_voltage_today(),   'V'),
+            ('Maximum Battery Voltage Today',         rover.battery_max_voltage_today(),   'V'),
+        ], f)
